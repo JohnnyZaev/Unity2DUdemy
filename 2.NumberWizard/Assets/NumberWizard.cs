635ed24511f914b4c1e813e4174683d4bbb766aa
gvarys <johnnyzaev@gmail.com>
2022-10-26 13:14:33 +0600

    NumberWizard is done

--- 2.NumberWizard/Assets/NumberWizard.cs
+++ 2.NumberWizard/Assets/NumberWizard.cs
@@ -2,15 +2,53 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private int _max;
+	private int _min;
+	private int _guess;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start ()
+	{
+		StartGame();
+	}
+
+	private void StartGame()
+	{
+		_max = 1000;
+		_min = 1;
+		_guess = 500;
+
+		Debug.Log("G'day mate, welcome to numba wiz-ud...");
+		Debug.Log("Pick a number, don't tell me what it is...");
+		Debug.Log("The highest number you can pick is: " + _max);
+		Debug.Log("The lowest number you can pick is: " + _min);
+		Debug.Log("Tell me if your number is higher or lower than: " + _guess);
+		Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+		_max += 1;
+	}
+
+	// Update is called once per frame
+	private void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			_min = _guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			_max = _guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Debug.Log("I am a genius!");
+			StartGame();
+		}
+	}
+
+	private void NextGuess()
+	{
+		_guess = (_max + _min) / 2;
+		Debug.Log("Is it higher or lower than..." + _guess);
+	}
 }
