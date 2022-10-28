8d831633cdff7bdfa27b470b86b75f5481a11aa0
gvarys <johnnyzaev@gmail.com>
2022-10-28 10:57:08 +0600

    text changes in code

---
+++ 3.Text101/Assets/AdventureGame.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+	public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    text.text = "I change this text";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
